566ef4ce0d41816459c50360d8cf3d1288e10d77
Andrea Miele <andrea.miele.pro@gmail.com>
2025-04-21 15:45:59 +0200

    add horizontal bar plot

---
+++ docs/_static/snippets/plots/general_barh_plot.py
@@ -0,0 +1,23 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from paperviz import plot
+
+data = {
+    "No APM limit": {"Test Elo": 1392},
+    "200% APM limit": {"Test Elo": 1411},
+    "100% APM limit": {"Test Elo": 1540},
+    "50% APM limit": {"Test Elo": 1536},
+    "25% APM limit": {"Test Elo": 1419},
+    "10% APM limit": {"Test Elo": 1145},
+    "0% APM limit": {"Test Elo": 0},
+}
+fig, ax = plot("general_horizontal_bar_plot",
+    data,
+    xlabel="Test Elo",
+    ylabel="APM limits",
+    color_map={"Test Elo": "#FF7F0E"},
+    bar_height=0.45,
+    style_map={"Test Elo": ""},
+    save="general_barh_plot",
+)
+plt.show()
