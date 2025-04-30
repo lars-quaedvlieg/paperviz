fab0164343fc234431ae90f80c1bc0bbd5f6771c
Andrea Miele <andrea.miele.pro@gmail.com>
2025-04-30 09:54:30 +0200

    add confusion matrix

---
+++ docs/_static/snippets/plots/confusion_matrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from paperviz import plot
+
+# 1) Define a toy confusion matrix
+#    Rows = true classes, Columns = predicted classes
+cm = np.array([
+    [50,  2,  3],
+    [ 5, 45, 10],
+    [ 2,  8, 40],
+])
+
+# 2) Class labels
+labels = ["Class A", "Class B", "Class C"]
+
+# 3) Plot raw counts
+fig1, ax1 = plot(
+    "confusion_matrix",
+    cm=cm,
+    labels=labels,
+    figsize=(6, 5),
+    cmap="Oranges",
+    normalize=False,
+    fmt="d",
+    cbar=True,
+    save="confusion_matrix_raw"
+)
+plt.show()
